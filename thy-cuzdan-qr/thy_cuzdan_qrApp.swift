c69e369dedd204e712d2feee30b7ad8c84b53a0e
Alp Eren Gürle <alpgurle1@hotmail.com>
2024-05-22 20:39:56 +0300

    Initial Commit

---
+++ thy-cuzdan-qr/thy_cuzdan_qrApp.swift
@@ -0,0 +1,17 @@
+//
+//  thy_cuzdan_qrApp.swift
+//  thy-cuzdan-qr
+//
+//  Created by MacBook on 22.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct thy_cuzdan_qrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
